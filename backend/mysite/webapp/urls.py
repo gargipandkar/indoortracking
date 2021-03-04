9f251c2b2a5367861d890e0850d52e8c59aaa37e
Madhu-balaji-01 <madhub2303@gmail.com>
2021-03-05 03:00:36 +0800

    Editing urls.py

--- backend/mysite/webapp/urls.py
+++ backend/mysite/webapp/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.RegisterPage, name = 'register'),
+    path('login/', views.LoginPage, name="login"),  
+	path('logout/', views.LogoutUser, name="logout"),
     path('viewplans/', views.HomePageView.as_view(), name='home'),
     path('addplan/', views.AddPlanView.as_view(), name='addplan'),
     path('getplans/', views.getFloorplans, name='getplans')
