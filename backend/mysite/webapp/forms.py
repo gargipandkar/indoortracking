34a02fab006c3c551ab93f40a28d7e2cab4bb899
Madhu-balaji-01 <madhub2303@gmail.com>
2021-03-05 02:56:32 +0800

    Adding User authentication

--- backend/mysite/webapp/forms.py
+++ backend/mysite/webapp/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Floorplan
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class FloorplanForm(forms.ModelForm):
 
     class Meta:
         model = Floorplan
         fields = ['title', 'plan']
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1','password2']
--- backend/mysite/webapp/views.py
+++ backend/mysite/webapp/views.py
@@ -1,12 +1,52 @@
 from django.shortcuts import render, redirect
-
 from django.http import HttpResponse, JsonResponse
+
+# For User auth
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 from django.views.generic import ListView, CreateView
 from .models import Floorplan
-from .forms import FloorplanForm
+from .forms import FloorplanForm, CreateUserForm
 
 from django.urls import reverse_lazy
+
+
+# User registration
+def RegisterPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created")
+
+            return redirect('login')
+
+        
+    context = {'form':form}
+    return render(request, 'templates/register.html', context)
+
+# User login
+def LoginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password =request.POST.get("password")
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request,'Username or password is incorrect')
+            
+    context = {}
+    return render(request, 'templates/login.html', context)
+
+def LogoutUser(request):
+    logout(request)
+    return redirect('login')
 
 # Create your views here.
 class HomePageView(ListView):
