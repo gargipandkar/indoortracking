ed2ccbf2d1637bac986897ef1cfdd91a9a7fd542
Gargi <gargi.pandkar@gmail.com>
2021-03-26 17:21:42 +0800

    Fix input dimensions

--- backend/mysite/knnalgo.py
+++ backend/mysite/knnalgo.py
@@ -2,6 +2,8 @@
 from sklearn.neighbors import KNeighborsRegressor
 
 model = KNeighborsRegressor(weights='distance', p=1)
+filename = 'knn_model.sav'
+uniqueapls = []
 
 def parse_vector_string(vectorstr):
     vectorls = vectorstr.replace("[", "").replace("]", "").replace(" ", "")
@@ -14,11 +16,12 @@ def parse_point_string(pointstr):
     pointls = pointstr.split(",")
     return pointls
 
-def clean_vectors(vectordictls):
+def get_superset(vectordictls):
     # get superset of all APs scanned across floor in alphabetical order
     uniqueapls = list(set(k for vectordict in vectordictls for k in vectordict.keys()))
     uniqueapls.sort()
 
+def clean_vectors(vectordictls):
     # a vector is a list of the RSSI value of each AP in the superset (and 0 if AP was not detected at a point)
     result = []
     for item in vectordictls:
@@ -29,6 +32,8 @@ def clean_vectors(vectordictls):
     
     return result
 
+def remove_aps(testdict):
+    return dict([(key, val) for key, val in testdict.items() if key in uniqueapls])
 
 def train_model(scanmap):
     #x --> vectors of RSSI values
@@ -43,14 +48,20 @@ def train_model(scanmap):
         pointstr = item['point']
         y.append(parse_point_string(pointstr))
 
+    get_superset(temp_x)
     x = clean_vectors(temp_x)
-    # return x
 
     model.fit(x, y)
+    # save the model to disk
+    pickle.dump(model, open(filename, 'wb'))
+   
     return True
 
 def get_prediction(testvector):
+    # load the model from disk
+    # loaded_model = pickle.load(open(filename, 'rb'))
     testvector = parse_vector_string(testvector)
+    testvector = remove_aps(testvector)
     testvector = clean_vectors([testvector])
     result = model.predict(testvector)
     return result
