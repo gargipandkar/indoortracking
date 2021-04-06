9b34b9a76b793339f6138c4363a36cc817e9da43
Gargi <gargi.pandkar@gmail.com>
2021-04-06 12:01:29 +0800

    Add POST request for current plan name

--- androidapp/app/src/main/java/com/example/scan_test/APIRequests.java
+++ androidapp/app/src/main/java/com/example/scan_test/APIRequests.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.wifi.ScanResult;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 
 import com.android.volley.NetworkResponse;
@@ -13,19 +14,27 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.google.api.gax.httpjson.HttpJsonCallOptions;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import java.util.HashMap;
 
 public class APIRequests {
-    public static Request<JSONArray> getAllPlans(Context ctx, String base_url, HashMap<String, String> allPlans){
+
+    private static HashMap<String, String> allPlans = new HashMap<>();
+
+    public static HashMap<String, String> getAllPlans() {
+        return allPlans;
+    }
+
+    public static Request<JSONArray> getAllPlans(Context ctx, String base_url){
         ProgressDialog progressDialog = new ProgressDialog(ctx);
         progressDialog.setMessage("Loading...");
         progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         progressDialog.show();
         progressDialog.setCancelable(true);
 
-        String url = base_url+"/api";
+        String url = base_url+"/getplans";
         String bucket_url = "https://storage.googleapis.com/floorplan-images/";
         JsonArrayRequest getRequest = new JsonArrayRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONArray>() {
@@ -55,14 +64,51 @@ public class APIRequests {
         return getRequest;
     }
 
-    public static StringRequest login(Context ctx, String base_url, String username, String password){
+    public static StringRequest sendCurrentPlan(Context ctx, String base_url, String currentPlan){
+        
         ProgressDialog progressDialog = new ProgressDialog(ctx);
         progressDialog.setMessage("Loading...");
         progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         progressDialog.show();
         progressDialog.setCancelable(true);
 
-        String url = base_url+"/login";
+        String url = base_url+"/getplans";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        Log.i("STRING RESPONSE", "--->" + response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("RESPONSE ERROR", error.toString());
+                    }
+                }
+                ) {
+                    @Override
+                    protected HashMap<String, String> getParams() {
+                        HashMap<String, String> params = new HashMap<String, String>();
+                        params.put("plan", currentPlan);
+                        return params;
+                    }
+        };
+
+        return postRequest;
+    }
+
+
+    public static StringRequest login(Context ctx, String base_url, String username, String password){
+        // TODO: handle authentication failure from server by prompting user again
+        ProgressDialog progressDialog = new ProgressDialog(ctx);
+        progressDialog.setMessage("Loading...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+        progressDialog.setCancelable(true);
+
+        String url = base_url+"/loginmobile";
         StringRequest postRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
@@ -90,22 +136,20 @@ public class APIRequests {
         return postRequest;
     }
 
-    public static Request<JSONArray> sendMapping(Context ctx, String base_url){
+    public static Request<JSONArray> sendMapping(Context ctx, String base_url, JSONArray mapped_data){
         ProgressDialog progressDialog = new ProgressDialog(ctx);
         progressDialog.setMessage("Loading...");
         progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         progressDialog.show();
         progressDialog.setCancelable(true);
 
-        JSONArray mapped_data = null;
-
-        String url = base_url+"/api";
+        String url = base_url+"/savemapping";
         JsonArrayRequest postRequest = new JsonArrayRequest(Request.Method.POST, url, mapped_data,
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
                         progressDialog.dismiss();
-                        Log.i("STRING RESPONSE", "--->" + response);
+                        Log.i("JSON RESPONSE", "--->" + response);
                     }
                 },
                 new Response.ErrorListener() {
